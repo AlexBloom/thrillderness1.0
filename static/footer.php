5da898744e90e02c54d4252042dac4db52037160
Alex Bloom <alexbloomdesign@gmail.com>
2014-09-22 17:56:47 -0400

    Added Footer & Sass

---
+++ static/footer.php
@@ -0,0 +1,90 @@
+
+</div> <!-- / end page container, begun in the header -->
+
+	<footer id="site-footer" role="contentinfo">
+		<div class="col2 clearfix">
+			<div>
+				<h4>Contact Us</h4>
+				<p class="intro" id="big-email"><a href="mailto:info@thrillderness.org">info@thrillderness.org</a></p>
+				<nav id="social">
+					<ul>
+						<li><a href="http://instagram.com/thrillderness"> <span class="icon icon-instagram"></span> <br>Instagram</a> </li>
+						<li><a href="http://twitter.com/thrillderness">	 <span class="icon icon-twitter"></span> 	<br>Twitter</a>   </li>
+						<li><a href="http://facebook.com/thrillderness"> <span class="icon icon-facebook"></span> 	<br>Facebook</a>  </li>
+						<li><a href="http://vimeo.com/thrillderness"> <span class="icon icon-vimeo"></span> 	<br>Vimeo</a>  </li>
+					</ul>
+				</nav>
+				
+			</div>		
+			<div>
+				<!-- Begin MailChimp Signup Form -->
+				<div id="mc_embed_signup">
+				<form action="http://thrillderness.us3.list-manage1.com/subscribe/post?u=5f27a954a3e91c470832a2ec7&amp;id=d8e1ac1bff" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+					<h4>Thrilldernews</h4>
+					<div class="indicates-required clearfix"><span class="asterisk">*</span> Required </div>
+						<div class="mc-field-group" id="emailaddress">
+							<label for="mce-EMAIL"> Email Address  <span class="asterisk">*</span> </label>
+							<input type="email" value="" name="EMAIL" placeholder="you@gmail.com" class="required email" id="mce-EMAIL">
+						</div>
+					
+						<div class="mc-field-group" id="firstname">
+							<label for="mce-FNAME"> First Name </label>
+							<input type="text" value="" name="FNAME" placeholder="Thrilly" class="" id="mce-FNAME">
+						</div>
+						<div class="mc-field-group" id="lastname">
+							<label for="mce-LNAME"> Last Name </label>
+							<input type="text" value="" name="LNAME" placeholder="Dernesser" class="" id="mce-LNAME">
+						</div>
+					
+					<!-- <div class="mc-field-group input-group">
+					    <strong>Email Format </strong>
+					    <ul class="radio-list">
+							<li><input type="radio" value="html" name="EMAILTYPE" id="mce-EMAILTYPE-0"checked><label for="mce-EMAILTYPE-0">html</label></li>
+							<li><input type="radio" value="text" name="EMAILTYPE" id="mce-EMAILTYPE-1"><label for="mce-EMAILTYPE-1">text</label></li>
+						</ul>
+					</div> -->
+						<div id="mce-responses" class="clearfix">
+							<div class="response" id="mce-error-response" style="display:none"></div>
+							<div class="response" id="mce-success-response" style="display:none"></div>
+						</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+					    <div style="position: absolute; left: -5000px;"><input type="text" name="b_5f27a954a3e91c470832a2ec7_d8e1ac1bff" value=""></div>
+						<div class="clear">
+							<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+						</div>
+				</form>
+				</div>
+				<!--End mc_embed_signup-->			
+			</div>
+		</div>
+		<nav id="footer-nav">
+			<ul class="clearfix">
+				<li>Manifesto</li>
+				<li>Who We Are</li>
+				<li>Purpose</li>
+			</ul>
+		</nav>
+		<div id="copyright" class="clearfix">
+			<small>We believe in open and free knowledge, so we license our content accordingly. </small>
+		</div>
+	</footer><!-- #site-footer -->
+
+ 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+
+	<script src="js/plugins.js"></script>
+	<script type="text/javascript" src="js/jquery.scrollTo.min.js"></script>
+	<script type="text/javascript" src="js/jquery.localScroll.min.js"></script>
+	<script src="js/swipe.js"></script>
+	<script src="js/main.js"></script>
+	
+	
+
+	<script>
+	    var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+	    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+	    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+	    s.parentNode.insertBefore(g,s)}(document,'script'));
+	</script>
+
+</body>
+</html>
