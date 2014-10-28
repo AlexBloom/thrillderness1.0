397a9026ba9ca252f8c81b315bf53dda3bc1e647
Alex Bloom <alexbloomdesign@gmail.com>
2014-10-28 19:26:20 -0400

    Feels Images

--- promo/index.php
+++ promo/index.php
@@ -1,7 +1,7 @@
 <?php include_once 'header.php' ?>	
 	
-<section class="clearfix">
-	<h1><img src="img/thrillderness_logo_animated.gif" alt="Thrillderness"/></h1>
+<section id="intro" class="clearfix">
+	<h1 id="logo"><img src="img/thrillderness_logo.gif" alt="Thrillderness"/></h1>
 	<h2>Educational, experimental and comical primers for outdoor adventures.<br>
 		<strong>Going 2015.</strong>
 	</h2>
@@ -13,19 +13,37 @@
 
 <section class="clearfix">
 	<h2>It's still a work in progress, but it's going to feel like this</h2>
-	<figure class="third">
-		<img src="img/feels/imagename.jpg" alt="Books" />
+	<figure class="half">
+		<img src="img/feels/smokey.jpg" alt="Fun" />
+		<figcaption>
+			<h3>Fun First</h3>
+		</figcaption>
+	</figure>
+	<figure class="half">
+		<img src="img/feels/jaeger_lynx.jpg" alt="Books" />
+		<figcaption>
+			<h3> </h3>
+		</figcaption>
+	</figure>
+	<figure class="half">
+		<img src="img/feels/oysterdome.jpg" alt="Adventures" />
 		<figcaption>
 			<h3></h3>
 		</figcaption>
 	</figure>
-	<figure class="third">
-		<img src="img/feels/imagename.jpg" alt="Fun" />
+	<figure class="half">
+		<img src="img/feels/jaeger_lynx.jpg" alt="Books" />
 		<figcaption>
 			<h3></h3>
 		</figcaption>
 	</figure>
-	<figure class="third">
+	<figure class="half">
+		<img src="img/feels/smokey.jpg" alt="Fun" />
+		<figcaption>
+			<h3></h3>
+		</figcaption>
+	</figure>
+	<figure class="half">
 		<img src="img/feels/imagename.jpg" alt="Adventures" />
 		<figcaption>
 			<h3></h3>
@@ -35,14 +53,16 @@
 
 <section class="clearfix">
 	<h2>and it's largely inspired by this:</h2>
-	<figure class="full">
-		<blockquote>
-			In wildness is the preservation of the world. 
-		</blockquote>
-		<figcaption>
-			<strong>Henry David Thoreau,</strong> <em><a href="http://thoreau.eserver.org/walking.html"target=_blank>Walking</a></em>, 1862
-		</figcaption>
-	</figure>
+	<section id="quote" class="full-screen-container full clearfix">
+		<figure class="full-screen-content">
+			<blockquote>
+				In wildness is the preservation of the world. 
+			</blockquote>
+			<figcaption>
+				<strong>Henry David Thoreau,</strong> <em><a href="http://thoreau.eserver.org/walking.html"target=_blank>Walking</a></em>, 1862
+			</figcaption>
+		</figure>
+	</section>
 </section>
 
 <section class="clearfix">
@@ -60,7 +80,7 @@
 		</figcaption>
 	</figure>
 	<figure class="third">
-		<img src="img/feels/imagename.jpg" alt="Adventures" />
+		<img src="img/feels/survivalist.jpg" alt="Adventures" />
 		<figcaption>
 			<h3>but with confidence in humanity</h3>
 		</figcaption>
@@ -72,7 +92,27 @@
 	<h2>If you made it this far, we hope you&rsquo;re as psyched as we are. <br>
 		<strong>Stay tuned.</strong>
 	</h2>
-	{mailchimp}
+	
+	<!-- Begin MailChimp Signup Form -->
+	<link href="//cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
+	<style type="text/css">
+		#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+		/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+		   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+	</style>
+	<div id="mc_embed_signup">
+	<form action="//thrillderness.us3.list-manage.com/subscribe/post?u=5f27a954a3e91c470832a2ec7&amp;id=d8e1ac1bff" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+	    <div id="mc_embed_signup_scroll">
+		<label for="mce-EMAIL">Let me know when it's ready</label>
+		<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+	    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+	    <div style="position: absolute; left: -5000px;"><input type="text" name="b_5f27a954a3e91c470832a2ec7_d8e1ac1bff" tabindex="-1" value=""></div>
+	    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+	    </div>
+	</form>
+	</div>
+
+	<!--End mc_embed_signup-->
 	<nav>
 		<ul class="clearfix">
 			<li> <a href="http://instagram.com/thrillderness"target=_blank> <span class="icon-instagram"> </span> </a> </li>
