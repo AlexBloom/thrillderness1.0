c7d97a6781951e6cd9c0c51abb9d341abfc732f2
Alex Bloom <alexbloomdesign@gmail.com>
2014-10-28 16:57:14 -0400

    New Index

---
+++ promo/index.php
@@ -0,0 +1,87 @@
+<?php include_once 'header.php' ?>	
+	
+<section class="clearfix">
+	<h1><img src="img/thrillderness_logo_animated.gif" alt="Thrillderness"/></h1>
+	<h2>Educational, experimental and comical primers for outdoor adventures.<br>
+		<strong>Going 2015.</strong>
+	</h2>
+	<nav .g1>
+		<a href="#" class="button">Learn More</a>
+		<a href="#" class="button">Sign Up</a>
+	</nav>
+</section>
+
+<section class="clearfix">
+	<h2>It's still a work in progress, but it's going to feel like this</h2>
+	<figure class="third">
+		<img src="img/feels/imagename.jpg" alt="Books" />
+		<figcaption>
+			<h3></h3>
+		</figcaption>
+	</figure>
+	<figure class="third">
+		<img src="img/feels/imagename.jpg" alt="Fun" />
+		<figcaption>
+			<h3></h3>
+		</figcaption>
+	</figure>
+	<figure class="third">
+		<img src="img/feels/imagename.jpg" alt="Adventures" />
+		<figcaption>
+			<h3></h3>
+		</figcaption>
+	</figure>
+</section>
+
+<section class="clearfix">
+	<h2>and it's largely inspired by this:</h2>
+	<figure class="full">
+		<blockquote>
+			In wildness is the preservation of the world. 
+		</blockquote>
+		<figcaption>
+			<strong>Henry David Thoreau,</strong> <em><a href="http://thoreau.eserver.org/walking.html"target=_blank>Walking</a></em>, 1862
+		</figcaption>
+	</figure>
+</section>
+
+<section class="clearfix">
+	<h2>and if we're dreaming big, it could be this:</h2>
+	<figure class="third">
+		<img src="img/feels/imagename.jpg" alt="Fun" />
+		<figcaption>
+			<h3>but gender and age inclusive</h3>
+		</figcaption>
+	</figure>
+	<figure class="third">
+		<img src="img/feels/imagename.jpg" alt="Books" />
+		<figcaption>
+			<h3>but, accessible and affordable</h3>
+		</figcaption>
+	</figure>
+	<figure class="third">
+		<img src="img/feels/imagename.jpg" alt="Adventures" />
+		<figcaption>
+			<h3>but with confidence in humanity</h3>
+		</figcaption>
+	</figure>
+</section>
+
+
+<section class="clearfix">
+	<h2>If you made it this far, we hope you&rsquo;re as psyched as we are. <br>
+		<strong>Stay tuned.</strong>
+	</h2>
+	{mailchimp}
+	<nav>
+		<ul class="clearfix">
+			<li> <a href="http://instagram.com/thrillderness"target=_blank> <span class="icon-instagram"> </span> </a> </li>
+			<li> <a href="http://tumblr.com/thrillderness"target=_blank> <span class="icon-tumblr"> </span> </a> </li>
+			<li> <a href="http://twitter.com/thrillderness"target=_blank> <span class="icon-twitter"> </span> </a> </li>
+		</ul>
+	</nav>
+</section>
+
+
+
+<?php include_once 'footer.php' ?>
