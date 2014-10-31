f331158848d712834cbff70998311dadcd7d331d
Alex Bloom <alexbloomdesign@gmail.com>
2014-10-31 15:06:19 -0400

    New Headers & Images

--- promo/index.php
+++ promo/index.php
@@ -1,50 +1,63 @@
 <?php include_once 'header.php' ?>	
 	
-<section id="intro" class="clearfix">
-	<h1 id="logo"><img src="img/thrillderness_logo.gif" alt="Thrillderness"/></h1>
-	<h2>Educational, experimental and comical primers for outdoor adventures.<br>
-		<strong>Going 2015.</strong>
-	</h2>
-	<nav .g1>
-		<a href="#" class="button">Learn More</a>
-		<a href="#" class="button">Sign Up</a>
-	</nav>
+<section id="intro" class="full-screen-container clearfix">
+	<section class="full-screen-content">
+		<h1 id="logo"><img src="img/Thrillderness-01.gif" alt="Thrillderness"/></h1>
+		
+		<!-- <div class="image-filter" data-stellar-background-ratio=".85" data-stellar-vertical-offset="-750" data-stellar-horizontal-offset="40">
+			<div id="logo-mask">
+				<h1> Thrillderness </h1>
+			</div>
+		</div> -->
+		
+		<h2>Educational, experimental and comical primers for outdoor adventures.<br>
+			<strong>Going 2015.</strong>
+		</h2>
+	
+		<nav id="primary">
+			<ul>
+				<li><a href="#feels">Learn More</a></li>
+				<li><a href="#sign-up">Sign Up</a>  </li>
+			</ul>
+		</nav>
+	</section>
 </section>
 
-<section class="clearfix">
-	<h2>It's still a work in progress, but it's going to feel like this</h2>
-	<figure class="half">
+<section id="feels" class="clearfix">
+	<h2>It's still a work in progress, but it's probably going to feel like a bit like this</h2>
+	<figure class="">
+		<img src="img/feels/sixmilecircle.jpg" alt="Fun" />
+		<figcaption>
+			<h3></h3>
+		</figcaption>
+	</figure>
+	<figure class="">
 		<img src="img/feels/smokey.jpg" alt="Fun" />
 		<figcaption>
-			<h3>Fun First</h3>
+			<h3></h3>
 		</figcaption>
 	</figure>
-	<figure class="half">
-		<img src="img/feels/jaeger_lynx.jpg" alt="Books" />
-		<figcaption>
-			<h3> </h3>
-		</figcaption>
-	</figure>
-	<figure class="half">
+	<figure class="">
 		<img src="img/feels/oysterdome.jpg" alt="Adventures" />
 		<figcaption>
 			<h3></h3>
 		</figcaption>
 	</figure>
-	<figure class="half">
-		<img src="img/feels/jaeger_lynx.jpg" alt="Books" />
+	<figure class="">
+		<img src="img/feels/compass.jpg" alt="Books" />
+		<figcaption>
+			<h3> </h3>
+		</figcaption>
+	</figure>
+	<figure class="">
+		<img src="img/feels/axe.jpg" alt="Books" />
 		<figcaption>
 			<h3></h3>
 		</figcaption>
 	</figure>
-	<figure class="half">
-		<img src="img/feels/smokey.jpg" alt="Fun" />
-		<figcaption>
-			<h3></h3>
-		</figcaption>
-	</figure>
-	<figure class="half">
-		<img src="img/feels/imagename.jpg" alt="Adventures" />
+	
+	<figure class="">
+		<img src="img/feels/handshake.jpg" alt="Adventures" />
 		<figcaption>
 			<h3></h3>
 		</figcaption>
@@ -52,8 +65,8 @@
 </section>
 
 <section class="clearfix">
-	<h2>and it's largely inspired by this:</h2>
-	<section id="quote" class="full-screen-container full clearfix">
+	<h2>and it's partly inspired by this:</h2>
+	<section id="quote" class="half clearfix">
 		<figure class="full-screen-content">
 			<blockquote>
 				In wildness is the preservation of the world. 
@@ -61,65 +74,67 @@
 			<figcaption>
 				<strong>Henry David Thoreau,</strong> <em><a href="http://thoreau.eserver.org/walking.html"target=_blank>Walking</a></em>, 1862
 			</figcaption>
+			<!-- <img src="img/arrow_down_white.gif"> -->
 		</figure>
 	</section>
 </section>
 
 <section class="clearfix">
-	<h2>and if we're dreaming big, it could be this:</h2>
-	<figure class="third">
-		<img src="img/feels/imagename.jpg" alt="Fun" />
+	<h2>and down the path, it could be a little like these:</h2>
+	<figure class="half">
+		<a href="http://scouting.org"target=_blank><img src="img/feels/bsa.jpg" alt="Fun" /></a>
 		<figcaption>
-			<h3>but gender and age inclusive</h3>
+			<h3>but, gender and age inclusive</h3>
 		</figcaption>
 	</figure>
-	<figure class="third">
-		<img src="img/feels/imagename.jpg" alt="Books" />
+	<figure class="half">
+		<a href="http://nols.edu"target=_blank><img src="img/feels/nols.jpg" alt="Books" /></a>
 		<figcaption>
-			<h3>but, accessible and affordable</h3>
+			<h3>but, without draining your wallet</h3>
 		</figcaption>
 	</figure>
-	<figure class="third">
-		<img src="img/feels/survivalist.jpg" alt="Adventures" />
-		<figcaption>
-			<h3>but with confidence in humanity</h3>
-		</figcaption>
-	</figure>
+	
 </section>
 
 
-<section class="clearfix">
-	<h2>If you made it this far, we hope you&rsquo;re as psyched as we are. <br>
-		<strong>Stay tuned.</strong>
-	</h2>
+<section id="sign-up" class="full-screen-container clearfix">
+	<section class="full-screen-content">
+		<h2>If you made it this far, we hope you&rsquo;re as psyched as we are. <br>
+			<strong>Stay tuned.</strong>
+		</h2>
 	
-	<!-- Begin MailChimp Signup Form -->
-	<link href="//cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
-	<style type="text/css">
-		#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
-		/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
-		   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-	</style>
-	<div id="mc_embed_signup">
-	<form action="//thrillderness.us3.list-manage.com/subscribe/post?u=5f27a954a3e91c470832a2ec7&amp;id=d8e1ac1bff" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-	    <div id="mc_embed_signup_scroll">
-		<label for="mce-EMAIL">Let me know when it's ready</label>
-		<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-	    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-	    <div style="position: absolute; left: -5000px;"><input type="text" name="b_5f27a954a3e91c470832a2ec7_d8e1ac1bff" tabindex="-1" value=""></div>
-	    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-	    </div>
-	</form>
-	</div>
+		<!-- Begin MailChimp Signup Form -->
+		<div id="mc_embed_signup" class="frame clearfix">
+			<form action="//thrillderness.us3.list-manage.com/subscribe/post?u=5f27a954a3e91c470832a2ec7&amp;id=d8e1ac1bff" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate clearfix" target="_blank" novalidate>
 
-	<!--End mc_embed_signup-->
-	<nav>
-		<ul class="clearfix">
-			<li> <a href="http://instagram.com/thrillderness"target=_blank> <span class="icon-instagram"> </span> </a> </li>
-			<li> <a href="http://tumblr.com/thrillderness"target=_blank> <span class="icon-tumblr"> </span> </a> </li>
-			<li> <a href="http://twitter.com/thrillderness"target=_blank> <span class="icon-twitter"> </span> </a> </li>
-		</ul>
-	</nav>
+				<!-- <div class="indicates-required"><span class="asterisk">*</span>Required</div> -->
+				<div class="mc-field-group">
+					<label for="mce-EMAIL">Sign up and we'll let you know when we launch.<span class="asterisk">*</span> </label>
+					<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+				</div>
+	
+				<div id="mce-responses" class="clear">
+					<div class="response" id="mce-error-response" style="display:none"></div>
+					<div class="response" id="mce-success-response" style="display:none"></div>
+				</div>    
+				<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+		    
+				<div style="position: absolute; left: -5000px;"><input type="text" name="b_5f27a954a3e91c470832a2ec7_d8e1ac1bff" tabindex="-1" value=""></div>
+		
+			    <input type="submit" value="ALOHA" name="subscribe" id="mc-embedded-subscribe">
+			</form>
+		</div>
+		<!--End mc_embed_signup-->
+	
+		<h3>Until go time...</h3>
+		<nav id="social-links">
+			<ul class="clearfix">
+				<li> <a href="http://instagram.com/thrillderness"target=_blank> <span class="icon-instagram"> </span> </a> </li>
+				<li> <a href="http://campthrillderness.tumblr.com"target=_blank> <span class="icon-tumblr"> </span> </a> </li>
+				<li> <a href="http://twitter.com/thrillderness"target=_blank> <span class="icon-twitter"> </span> </a> </li>
+			</ul>
+		</nav>
+	</section>
 </section>
 
 
