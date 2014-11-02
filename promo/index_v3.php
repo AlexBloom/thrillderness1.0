2a321377ea2da1139947f225d6c3fea6bfa03c1f
Alex Bloom <alexbloomdesign@gmail.com>
2014-11-02 13:43:24 -0500

    Re-Organization

--- promo/index_v3.php
+++ promo/index_v3.php
@@ -1,18 +1,5 @@
 <?php include_once 'header.php' ?>	
-
-<section class="clearfix">
-	<section id="quote" class="half clearfix">
-		<figure class="full-screen-content">
-			<blockquote>
-				In wildness is the preservation of the world. 
-			</blockquote>
-			<figcaption>
-				<strong>Henry David Thoreau,</strong> <em><a href="http://thoreau.eserver.org/walking.html"target=_blank>Walking</a></em>, 1862
-			</figcaption>
-		</figure>
-	</section>
-</section>	
-
+	
 <section id="intro" class="full-screen-container clearfix">
 	<section class="full-screen-content">
 		<h1 id="logo"><img src="img/Thrillderness-01.gif" alt="Thrillderness"/></h1>
@@ -36,7 +23,7 @@
 	</section>
 </section>
 
-<!-- <section id="feels" class="clearfix"> 
+<section id="feels" class="clearfix">
 	<h2>It's still a work in progress, but it's probably going to feel like a bit like this</h2>
 	<figure class="">
 		<img src="img/feels/sixmilecircle.jpg" alt="Fun" />
@@ -75,9 +62,22 @@
 			<h3></h3>
 		</figcaption>
 	</figure>
-</section>-->
+</section>
 
-
+<section class="clearfix">
+	<h2>and it's partly inspired by this:</h2>
+	<section id="quote" class="half clearfix">
+		<figure class="full-screen-content">
+			<blockquote>
+				In wildness is the preservation of the world. 
+			</blockquote>
+			<figcaption>
+				<strong>Henry David Thoreau,</strong> <em><a href="http://thoreau.eserver.org/walking.html"target=_blank>Walking</a></em>, 1862
+			</figcaption>
+			<!-- <img src="img/arrow_down_white.gif"> -->
+		</figure>
+	</section>
+</section>
 
 <section class="clearfix">
 	<h2>and down the path, it could be a little like these:</h2>
@@ -99,11 +99,14 @@
 
 <section id="sign-up" class="full-screen-container clearfix">
 	<section class="full-screen-content">
+		<h2>If you made it this far, we hope you&rsquo;re as psyched as we are. <br>
+			<strong>Stay tuned.</strong>
+		</h2>
 	
 		<!-- Begin MailChimp Signup Form -->
 		<div id="mc_embed_signup" class="frame clearfix">
 			<form action="//thrillderness.us3.list-manage.com/subscribe/post?u=5f27a954a3e91c470832a2ec7&amp;id=d8e1ac1bff" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate clearfix" target="_blank" novalidate>
-			<p><a href="#">Include your address</a> for a welcome gift.</p>
+
 				<!-- <div class="indicates-required"><span class="asterisk">*</span>Required</div> -->
 				<div class="mc-field-group">
 					<label for="mce-EMAIL">Sign up and we'll let you know when we launch.<span class="asterisk">*</span> </label>
@@ -122,21 +125,15 @@
 			</form>
 		</div>
 		<!--End mc_embed_signup-->
-	</section>
-</section>
-
-<section id="bonus" class="full-screen-container clearfix">
-	<section class="full-screen-content">
-			<h2>If you made it this far, we hope you&rsquo;re as psyched as we are. <br>
-				<strong>Stay tuned.</strong>
-			</h2>
-			<nav id="social-links">
-				<ul class="clearfix">
-					<li> <a href="http://instagram.com/thrillderness"target=_blank> <span class="icon-instagram"> </span> </a> </li>
-					<li> <a href="http://campthrillderness.tumblr.com"target=_blank> <span class="icon-tumblr"> </span> </a> </li>
-					<li> <a href="http://twitter.com/thrillderness"target=_blank> <span class="icon-twitter"> </span> </a> </li>
-				</ul>
-			</nav>
+	
+		<h3>Until go time...</h3>
+		<nav id="social-links">
+			<ul class="clearfix">
+				<li> <a href="http://instagram.com/thrillderness"target=_blank> <span class="icon-instagram"> </span> </a> </li>
+				<li> <a href="http://campthrillderness.tumblr.com"target=_blank> <span class="icon-tumblr"> </span> </a> </li>
+				<li> <a href="http://twitter.com/thrillderness"target=_blank> <span class="icon-twitter"> </span> </a> </li>
+			</ul>
+		</nav>
 	</section>
 </section>
 
